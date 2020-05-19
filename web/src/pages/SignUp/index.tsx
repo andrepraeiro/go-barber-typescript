1a106e54a91b91306de9abc4f0eb77a9f8714743
Andre Praeiro <andrepraeiro@gmail.com>
2020-05-19 15:29:13 -0400

    sign up page

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiUser, FiLock, FiArrowDownLeft } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
@@ -7,26 +7,30 @@ import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => {
+const SignUp: React.FC = () => {
   return (
     <Container>
+      <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
         <form autoComplete="off">
-          <h1>Faça seu logon</h1>
+          <h1>Faça seu cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
-          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
-          <Button type="submit">Entrar</Button>
-          <a href="forgot">Esqueci minha senha</a>
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha" />
+          <Button type="submit">Cadastrar</Button>
         </form>
         <a href="">
-          <FiLogIn />
-          Criar conta
+          <FiArrowDownLeft />
+          Voltar para logon
         </a>
       </Content>
-      <Background />
     </Container>
   );
 };
 
-export default SignIn;
+export default SignUp;
