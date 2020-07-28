fc8e708145928986f055b251c3652ad559b99aac
Andre Praeiro <andrepraeiro@gmail.com>
2020-07-28 12:35:18 -0400

    routes validation

--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import { celebrate, Segments, Joi } from 'celebrate';
 import ProvidersController from '../Controllers/ProvidersControllers';
 import ProviderMonthAvailabilityController from '../Controllers/ProviderMonthAvailabilityController';
 import ProviderDayAvailabilityController from '../Controllers/ProviderDayAvailabilityController';
@@ -13,10 +14,20 @@ providersRouter.use(ensureAuthenticated);
 providersRouter.get('/', providerController.index);
 providersRouter.get(
   '/:provider_id/month-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provier_id: Joi.string().uuid().required(),
+    },
+  }),
   providerMonthAvailabilityController.index
 );
 providersRouter.get(
   '/:provider_id/day-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provier_id: Joi.string().uuid().required(),
+    },
+  }),
   providerDayAvailabilityController.index
 );
 
